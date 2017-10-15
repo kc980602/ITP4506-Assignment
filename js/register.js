3d8134ce891e77e3d9ef48c2cb8c65f9ddd7b82a
PokeGuy <pokeguys@users.noreply.github.com>
2017-10-15 22:40:48 +0800

    Added null check in registration

--- js/register.js
+++ js/register.js
@@ -2,13 +2,51 @@ $(document).ready(() => {
   $('#step2, #step3').hide()
 })
 
+$('#email, #username, #password, #password_confirmation').change(() => {
+  let username = $('#username').val(),
+  password = $('#password').val(),
+  password_confirmation = $('#password_confirmation').val(),
+  valid = true
+  if (!validateEmail($('#email').val())) {
+    $('#error').append('<li>Invalid Email address.</li>')
+    $('#email').addClass('invalid')
+    valid = false
+  } else {
+    $('#email').removeClass('invalid')
+  }
+  if (username.length < 6) {
+    $('#error').append('<li>Username cannot less than 6 characters.</li>')
+    $('#username').addClass('invalid')
+    valid = false
+  } else {
+    $('#username').removeClass('invalid')
+  }
+  if (password.length < 6) {
+    $('#error').append('<li>Password cannot less than 6 characters.</li>')
+    $('#password').addClass('invalid')
+    valid = false
+  } else {
+    $('#password').removeClass('invalid')
+  }
+  if (password != password_confirmation) {
+    $('#error').append('<li>Two password is not match.</li>')
+    $('#password_confirmation').addClass('invalid')
+    valid = false
+  } else {
+    $('#password_confirmation').removeClass('invalid')
+  }
+  if (valid) {
+    $('#registerNext').attr('disabled', false)
+  }
+})
+
 $('#registerBack').click(() => {
   let step = $('#registerBox').data('step')
   $('#registerNext').text('Next')
   $('#registerBack').attr('disabled', false)
+  $('#registerNext').attr('disabled', true)
   step--
   if (step <= 1) {
-    $('#registerNext').attr('disabled', false)
     $('#registerBack').attr('disabled', true)
     step = 1
   }
@@ -20,6 +58,7 @@ $('#registerBack').click(() => {
 })
 
 $('#registerNext').click(() => {
+  $('#registerNext').attr('disabled', true)
   let step = $('#registerBox').data('step')
   if (step === 3) {
     alert('Account registeration successful.')
@@ -36,3 +75,9 @@ $('#registerNext').click(() => {
     $('#registerBox').data('step', step)
   }
 })
+
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email)
+}
